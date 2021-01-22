61d10a11dd04ecdca4bfe4b5fe892d2992268fd2
FrankellySoftware <72563316+FrankellySoftware@users.noreply.github.com>
2021-01-22 16:31:06 -0400

    ajax

--- detrasdelaopinion web/main.py
+++ detrasdelaopinion web/main.py
@@ -1,10 +1,11 @@
 # pip install flask , flask-login , flask_mysql , gunicorn, pymongo
 import platform
 import json
-from pymongo import MongoClient,errors
+import socket
+from pymongo import MongoClient, errors
 import os
 import datetime
-from flask import render_template, request, redirect, url_for, Flask, flash
+from flask import render_template, make_response, request, redirect, url_for, Flask, flash,jsonify
 
 
 app = Flask(__name__)
@@ -14,9 +15,10 @@ app.config['SECRET_KEY'] = "fsdgsdashedgehertjdgbdfjndf"
 
 
 # ========== CONFIGURACION DE LA BBDD ===========
-host = MongoClient("mongodb+srv://frankellysoftware:Leon_crack2077@cluster0.bggvz.mongodb.net/DDLO?retryWrites=true&w=majority")
+host = MongoClient(
+    "mongodb+srv://frankellysoftware:Leon_crack2077@cluster0.bggvz.mongodb.net/DDLO?retryWrites=true&w=majority")
 db = host.get_database("DDLO")
-admin = db.admin
+suscription = db.suscripciones
 noticias = db.noticias
 
 
@@ -41,7 +43,7 @@ def getTituloUrl(titulo):
 
 def getDocumentosBBDD():
     documentos = noticias.find(
-        {}, {'_id': 0,'titulo_url': 1, 'titulo': 1, 'resumen': 1, 'imagenes': 1})
+        {}, {'_id': 0, 'titulo_url': 1, 'titulo': 1, 'resumen': 1, 'imagenes': 1})
 
     docs = []
 
@@ -50,6 +52,8 @@ def getDocumentosBBDD():
     return docs
 
 # ========== PAGINA DE ERROR 404 ===========
+
+
 @app.errorhandler(404)
 def error404(e):
     return render_template("404.html")
@@ -59,8 +63,8 @@ def error404(e):
 @app.route("/")
 def index():
     try:
-        a = noticias.find({},limit=3)
-        return render_template("index.html", relevantes = a)
+        a = noticias.find({}, limit=3)
+        return render_template("index.html", relevantes=a)
     except errors.AutoReconnect:
         return "<h1>Error al conectarse con el servidor - 500</h1>"
 # ========== PAGINA DE ACERCA DE NOSOTROS ===========
@@ -75,7 +79,7 @@ def acerca_de():
 @app.route("/noticias")
 def post():
     cards = getDocumentosBBDD()
-    return render_template("post.html" ,cards = cards)
+    return render_template("post.html", cards=cards)
 
 
 # ========== noticia ===========
@@ -92,7 +96,7 @@ def noticia(titulo_url):
 
                 la variable 'documento' procediente del servidor, contiene la informacion de la Noticia solicitada. 
                 Esta variable es de tipo Json o Dict en Python y contiene las siguentes Claves:
-                
+
                 - id
                 - titulo_url
                 - titulo
@@ -115,5 +119,24 @@ def contacto():
     return render_template("contacto.html")
 
 
+# ========== AJAX ROUTE ===========
+@app.route("/ajax_post", methods=['POST'])
+def appAJAX():
+    if request.method == "POST":
+        data = request.form['EMAIL']
+        response = {'correo': data,
+                    'fecha': datetime.datetime.now().strftime('%d/%m/%Y')}
+
+        for mail in suscription.find({}):
+            if mail['correo'] == data:
+                flash('El correo "%s" ya existe en nuestra base de datos.')
+                print('Existe el correo')
+                break
+            else:
+                suscription.insert_one(response)
+
+        return jsonify(response)
+
+
 if __name__ == "__main__":
-    app.run(host="10.0.0.50",debug=True)
+    app.run(host="10.0.0.50", debug=True)
---
+++ detrasdelaopinion web/static/js/ajax.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    const mail = () => {
+        
+        $.ajax({
+            type: "POST",
+            url:'/ajax_post',
+            data: $('form').serialize(),
+            success: function(response){
+                console.log(response);
+            },
+            error:function(error){
+                console.log("Error");
+            }
+        })
+    }
+
+    $('#email-form').submit(function(){
+        console.log("wooo");
+        mail();
+    })
+
+});
